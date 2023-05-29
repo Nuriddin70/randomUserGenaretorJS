65f9fc87d5a206d9b5ae69f8c984b972b9d1dd70
Nuriddin70 <nuriddin3050@gmail.com>
2023-05-29 19:17:07 +0900

    first commit

---
+++ js/request.js
@@ -0,0 +1,51 @@
+// const { request } = require("http")
+
+// api
+const API = "https://randomuser.me/api/?results=9";
+
+// for leader
+const overlay = document.getElementById("overlay");
+
+const loaderToggle = (toggle) => {
+  if (toggle) {
+    overlay.classList.remove("hidden");
+  } else {
+    overlay.classList.add("hidden");
+  }
+};
+
+//request promis
+const getData = (resourse) => {
+  return new Promise((resolve, reject) => {
+    const request = new XMLHttpRequest();
+
+    request.addEventListener("readystatechange", () => {
+      if (request.readystatechange < 4) {
+        loaderToggle(true);
+      } else if (request.readyState == 4 && request.status == 200) {
+        const data = JSON.parse(request.responseText);
+        resolve(data.results);
+        loaderToggle(false);
+      } else if (request.request == 4) {
+        reject("Error!!!");
+        loaderToggle(false);
+      }
+    });
+
+    request.open("GET", resourse);
+    request.send();
+  });
+};
+
+// load
+const reload = () => {
+  getData(API)
+    .then((data) => {
+      updateUI(data);
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+};
+
+document.addEventListener("DOMContentLoaded", reload);
